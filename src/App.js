82c9d31581776b091d0e120ca6fd1d593df05631
emonShimoul <emonshimoul@gmail.com>
2022-12-26 20:18:51 +0600

    create counter using react useState

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Counter from './components/Counter';
 
 function App() {
   return (
     <div className="App">
       <h2>React Redux</h2>
+      <Counter></Counter>
     </div>
   );
 }
