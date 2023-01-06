de947754bbac51c195d3e0340e0dbdf609300a13
emonShimoul <emonshimoul@gmail.com>
2023-01-06 23:18:32 +0600

    created all actions

--- src/services/actions/counterAction.js
+++ src/services/actions/counterAction.js
@@ -1,7 +1,19 @@
-import { INCREMENT } from "../constants/counterConstants"
+import { DECREMENT, INCREMENT, RESET } from "../constants/counterConstants"
 
 export const incrementCounter = () => {
     return {
         type: INCREMENT
+    }
+}
+
+export const decrementCounter = () => {
+    return {
+        type: DECREMENT
+    }
+}
+
+export const resetCounter = () => {
+    return {
+        type: RESET
     }
 }
