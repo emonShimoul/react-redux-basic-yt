a5fb1a1206f6e4a570577572bacd78c0ed2ffd7a
emonShimoul <emonshimoul@gmail.com>
2023-01-06 23:10:19 +0600

    remove the states

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,17 +1,16 @@
 import React, { useState } from 'react';
 
 const Counter = () => {
-    const [count, setCount] = useState(0);
 
     const handleIncrement = () => {
-        setCount(count + 1);
+
     };
 
     return (
         <div>
             <h2>Counter App</h2>
 
-            <h3>Count: {count}</h3>
+            <h3>Count: 0</h3>
             <button onClick={handleIncrement}>Increment</button>
         </div>
     );
